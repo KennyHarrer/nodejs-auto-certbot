179510b6d5df527c3c32ddc502fa013d93944652
Kenny Harrer <kenny.harrer@icloud.com>
2024-01-24 20:48:40 -0600

    use absolute path to hook scripts

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
+const path = require('path')
 
 //
 
@@ -12,13 +13,14 @@ app.use(express.static('public'));
 
 app.listen(async () => {
     await runCertbot();
+    process.exit()
 });
 
 // CERT BOT FUNC
 
 async function runCertbot() {
     try {
-        const certbotCommand = `certbot certonly --manual --preferred-challenges=http --manual-auth-hook auth.sh --manual-cleanup-hook cleanup.sh -d ${process.env.certbotDomain}`;
+        const certbotCommand = `certbot certonly --manual --preferred-challenges=http --manual-auth-hook ${path.join(__dirname,'auth.sh')} --manual-cleanup-hook ${path.join(__dirname,'cleanup.sh')} -d ${process.env.certbotDomain}`;
 
         const { stdout, stderr } = await exec(certbotCommand);
 
@@ -28,6 +30,6 @@ async function runCertbot() {
             console.error(`Certbot errors:\n${stderr}`);
         }
     } catch (error) {
-        console.error(`Error running Certbot: ${error}`);
+        console.error(`Error running Certbot: ${error}`);    
     }
 }
